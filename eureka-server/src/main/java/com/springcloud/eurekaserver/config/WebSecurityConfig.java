0097c4e4d164f2a57d87e0684226eccef6424127
guoqi39 <1051788373@qq.com>
2022-03-18 12:26:16 +0800

    注释代码

--- eureka-server/src/main/java/com/springcloud/eurekaserver/config/WebSecurityConfig.java
+++ eureka-server/src/main/java/com/springcloud/eurekaserver/config/WebSecurityConfig.java
@@ -1,20 +1,20 @@
-package com.springcloud.eurekaserver.config;
-
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-/**
- * @description 
- * @author Guoqi
- * @date 2022/03/18 10:48
- **/
-@EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        super.configure(httpSecurity);//为了访问eureka和actuator时作安全控制
-        httpSecurity.csrf().ignoringAntMatchers("/eureka/**"); //忽略/eureka/的请求
-    }
-}
+//package com.springcloud.eurekaserver.config;
+//
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+///**
+// * @description
+// * @author Guoqi
+// * @date 2022/03/18 10:48
+// **/
+//@EnableWebSecurity
+//public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//    @Override
+//    protected void configure(HttpSecurity httpSecurity) throws Exception {
+//        super.configure(httpSecurity);//为了访问eureka和actuator时作安全控制
+//        httpSecurity.csrf().ignoringAntMatchers("/eureka/**"); //忽略/eureka/的请求
+//    }
+//}
